f08208b91f70f62cbdbe764579981940e9b9acc6
messi091 <ap.amber91@gmail.com>
2021-05-30 19:06:57 +0530

    Initial data fetch app using SpringBatch.
    data fetched https://www.kaggle.com/patrickb1912/ipl-complete-dataset-20082020 
    and parsed from CSV to DB using SpringBatch

---
+++ src/test/java/com/projects/IPL_dashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.IPL_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
